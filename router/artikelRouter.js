f3a1ebd0990307417f9e2fd1a855560a37781baa
zero0dayali <officialali35@gmail.com>
2020-12-13 10:21:38 +0700

    Ini sudah bisa Update

--- router/artikelRouter.js
+++ router/artikelRouter.js
@@ -3,7 +3,8 @@ const router = express.Router();
 const artikelController = require("../controller/artikelController")
 
 router.get('/',artikelController.index)
-router.get('/tambah',artikelController.create);
-router.post('/',artikelController.store);
-
+router.get('/tambah',artikelController.create)
+router.post('/',artikelController.store)
+router.get('/edit/:id',artikelController.edit)
+router.put('/:id',artikelController.update);
 module.exports = router
